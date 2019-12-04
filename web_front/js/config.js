d74e5bdd9d5de7c5f7e2dbf8bfe5ed399d56909f
liubing <1184679882@qq.com>
2019-12-04 10:25:40 +0800

    游客添加评论

--- web_front/js/config.js
+++ web_front/js/config.js
@@ -2,6 +2,6 @@ var baseUrl = 'http://localhost:8000/'
 
 var APIURLS = {
     category_show: baseUrl + 'admin/category_search',
-    article_show: baseUrl + 'admin/search'
-
+    article_show: baseUrl + 'admin/search',
+    comment_add: baseUrl + 'post_comment'
 }
